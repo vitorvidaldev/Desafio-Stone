e82284c5c446bd2cbb8386a0d09368ef25cedf45
Vitor <vitorvidal.dev@gmail.com>
2021-02-06 10:13:19 -0300

    Finalização da leitura dos arquivos e atualização da documentação

--- Main.java
+++ Main.java
@@ -11,8 +11,9 @@ public class Main {
         List<Item> items = new ArrayList<Item>();
         List<String> emails = new ArrayList<String>();
 
+        // List initialization
         try {
-            // Leitura da lista de emails
+            // Reading email list
             BufferedReader emailReader = new BufferedReader(new FileReader("emails.txt"));
 
             String line;
@@ -21,17 +22,42 @@ public class Main {
             }
             emailReader.close();
 
-            // Leitura da lista de itens
+            // Reading items list
             BufferedReader itemsReader = new BufferedReader(new FileReader("items.txt"));
 
             while ((line = itemsReader.readLine()) != null) {
-                System.out.println(line);
+                String[] parts = line.split(";");
+
+                Item newItem = new Item();
+                newItem.name = parts[0];
+                newItem.value = Integer.parseInt(parts[1]);
+                newItem.quantity = Integer.parseInt(parts[2]);
+
+                items.add(newItem);
             }
             itemsReader.close();
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        // Calculate value per person.
+        Map<String, Integer> result = valuePerPerson(emails, items);
+    }
+
+    public static Map<String, Integer> valuePerPerson(List<String> emails, List<Item> items) {
+        int totalValue = 0;
+
+        for (Item i : items) {
+            totalValue += i.value * i.quantity;
+        }
+
+        int finalValue = totalValue / emails.size();
+
+        System.out.println(totalValue);
+        System.out.println(finalValue);
+
+        return null;
     }
 }
 
